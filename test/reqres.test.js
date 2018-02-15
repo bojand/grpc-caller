c958ba6f4b2f5224e21c87de8df924f14d5841b1
Bojan Djurkovic <dbojan@gmail.com>
2018-02-14 22:46:12 -0400

    Request API req res tests

--- test/reqres.test.js
+++ test/reqres.test.js
@@ -25,6 +25,11 @@ const client = caller(DYNAMIC_HOST, PROTO_PATH, 'ArgService')
 test.before('should dynamically create service', t => {
   function doSomething (call, callback) {
     const ret = { message: call.request.message }
+
+    const md = new grpc.Metadata()
+    md.set('headerMD', 'headerValue')
+    call.sendMetadata(md)
+
     if (call.metadata) {
       const meta = call.metadata.getMap()
       if (meta['user-agent']) {
@@ -34,7 +39,11 @@ test.before('should dynamically create service', t => {
         ret.metadata = JSON.stringify(meta)
       }
     }
-    callback(null, ret)
+
+    const md2 = new grpc.Metadata()
+    md2.set('trailerMD', 'trailerValue')
+
+    callback(null, ret, md2)
   }
 
   const server = new grpc.Server()
@@ -224,6 +233,144 @@ test('Request API: call service using async with just an argument', async t => {
   t.truthy(response.message)
   t.falsy(response.metadata)
   t.is(response.message, 'Hi')
+})
+
+test.cb('Request API: call service using callback with metadata as plain object', t => {
+  t.plan(9)
+  const ts = new Date().getTime()
+  const req = client
+    .request('doSomething', { message: 'Hello' })
+    .setMetadata({ requestId: 'bar-123', timestamp: ts })
+
+  req.exec((err, res) => {
+    t.ifError(err)
+    const { response } = res
+    t.truthy(res.call)
+    t.falsy(res.metadata)
+    t.falsy(res.status)
+    t.truthy(response)
+    t.truthy(response.message)
+    t.is(response.message, 'Hello')
+    t.truthy(response.metadata)
+    const metadata = JSON.parse(response.metadata)
+    const expected = { requestid: 'bar-123', timestamp: ts.toString() }
+    t.deepEqual(metadata, expected)
+    t.end()
+  })
+})
+
+test('Request API: call service using async with metadata as plain object', async t => {
+  t.plan(8)
+  const ts = new Date().getTime()
+
+  const req = client
+    .request('doSomething', { message: 'Hi' })
+    .setMetadata({ requestId: 'bar-123', timestamp: ts })
+
+  const res = await req.exec()
+  t.truthy(res.call)
+  t.falsy(res.metadata)
+  t.falsy(res.status)
+  const { response } = res
+  t.truthy(response)
+  t.truthy(response.message)
+  t.is(response.message, 'Hi')
+  t.truthy(response.metadata)
+  const metadata = JSON.parse(response.metadata)
+  const expected = { requestid: 'bar-123', timestamp: ts.toString() }
+  t.deepEqual(metadata, expected)
+})
+
+test('Request API: with metadata option', async t => {
+  t.plan(9)
+  const ts = new Date().getTime()
+
+  const req = client
+    .request('doSomething', { message: 'Hi' })
+    .setMetadata({ requestId: 'bar-123', timestamp: ts })
+    .withResponseMetadata(true)
+
+  const res = await req.exec()
+  t.truthy(res.call)
+  t.truthy(res.metadata)
+  const md1 = res.metadata.getMap()
+  const expectedMd = { headermd: 'headerValue' }
+  t.deepEqual(md1, expectedMd)
+
+  t.falsy(res.status)
+  const { response } = res
+  t.truthy(response)
+  t.truthy(response.message)
+  t.is(response.message, 'Hi')
+  t.truthy(response.metadata)
+  const metadata = JSON.parse(response.metadata)
+  const expected = { requestid: 'bar-123', timestamp: ts.toString() }
+  t.deepEqual(metadata, expected)
+})
+
+test('Request API: with status option', async t => {
+  t.plan(12)
+  const ts = new Date().getTime()
+
+  const req = client
+    .request('doSomething', { message: 'Hi' })
+    .setMetadata({ requestId: 'bar-123', timestamp: ts })
+    .withResponseStatus(true)
+
+  const res = await req.exec()
+  t.truthy(res.call)
+  t.falsy(res.metadata)
+  t.truthy(res.status)
+  t.is(res.status.code, 0)
+  t.is(res.status.details, 'OK')
+  t.truthy(res.status.metadata)
+  const md1 = res.status.metadata.getMap()
+  const expectedMd = { trailermd: 'trailerValue' }
+  t.deepEqual(md1, expectedMd)
+
+  const { response } = res
+  t.truthy(response)
+  t.truthy(response.message)
+  t.is(response.message, 'Hi')
+  t.truthy(response.metadata)
+  const metadata = JSON.parse(response.metadata)
+  const expected = { requestid: 'bar-123', timestamp: ts.toString() }
+  t.deepEqual(metadata, expected)
+})
+
+test('Request API: with metadata and status option', async t => {
+  t.plan(12)
+  const ts = new Date().getTime()
+
+  const req = client
+    .request('doSomething', { message: 'Hi' })
+    .setMetadata({ requestId: 'bar-123', timestamp: ts })
+    .withResponseMetadata(true)
+    .withResponseStatus(true)
+
+  const res = await req.exec()
+
+  t.truthy(res.metadata)
+  const md1 = res.metadata.getMap()
+  const expectedMd = { headermd: 'headerValue' }
+  t.deepEqual(md1, expectedMd)
+
+  t.truthy(res.status)
+  t.is(res.status.code, 0)
+  t.is(res.status.details, 'OK')
+  t.truthy(res.status.metadata)
+  const md2 = res.status.metadata.getMap()
+  const expectedMd2 = { trailermd: 'trailerValue' }
+  t.deepEqual(md2, expectedMd2)
+
+  const { response } = res
+  t.truthy(response)
+  t.truthy(response.message)
+  t.is(response.message, 'Hi')
+  t.truthy(response.metadata)
+  const metadata = JSON.parse(response.metadata)
+  const expected = { requestid: 'bar-123', timestamp: ts.toString() }
+  t.deepEqual(metadata, expected)
 })
 
 test.after.always.cb('guaranteed cleanup', t => {
